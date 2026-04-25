4e5bbe941ebff22e4cc53d92eeded1aca41ebd11
MalinduGurunada <it23736528@my.sliit.lk>
2026-04-26 00:35:04 +0530

    feat(k6): add ramping-vus scenario skeleton for write load

--- performance/k6/post-releases.js
+++ performance/k6/post-releases.js
@@ -2,3 +2,12 @@ import http from 'k6/http';
 import { check } from 'k6';
 import { BASE_URL, authHeaders } from './common/config.js';
 import { ensureProduct, loginAsAdmin } from './common/auth.js';
+
+export const options = {
+  scenarios: {
+    write_load: {
+      executor: 'ramping-vus',
+      stages: []
+    }
+  }
+};
