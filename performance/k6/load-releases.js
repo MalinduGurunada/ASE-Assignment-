e6caabd3c5c633ac527a45939fc43b087faeb9f3
MalinduGurunada <it23736528@my.sliit.lk>
2026-04-26 00:34:10 +0530

    feat(k6): add constant-vus scenario (75 VUs, 2 minutes)

--- performance/k6/load-releases.js
+++ performance/k6/load-releases.js
@@ -2,3 +2,13 @@ import http from 'k6/http';
 import { check } from 'k6';
 import { BASE_URL, SHARED_THRESHOLDS, authHeaders } from './common/config.js';
 import { loginAsAdmin } from './common/auth.js';
+
+export const options = {
+  scenarios: {
+    steady_release_reads: {
+      executor: 'constant-vus',
+      vus: 75,
+      duration: '2m'
+    }
+  }
+};
