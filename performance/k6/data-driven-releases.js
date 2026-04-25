1b102aae5d1b0178140f3f1ed9fa8d4c5af0797a
MalinduGurunada <it23736528@my.sliit.lk>
2026-04-26 00:37:26 +0530

    feat(k6): scaffold data-driven-releases.js with SharedArray import

---
+++ performance/k6/data-driven-releases.js
@@ -0,0 +1,5 @@
+import http from 'k6/http';
+import { check } from 'k6';
+import { SharedArray } from 'k6/data';
+import { BASE_URL, SHARED_THRESHOLDS, authHeaders } from './common/config.js';
+import { ensureProduct, loginAsAdmin } from './common/auth.js';
