2947bf74d8b59406446aa735df238d336771dcb2
it23619012 <it23619012@my.sliit.lk>
2026-04-26 09:54:01 +0530

    feat(e2e): scaffold rbac.spec.ts with imports and describe block

---
+++ frontend/e2e/specs/rbac.spec.ts
@@ -0,0 +1,6 @@
+import { test } from '@playwright/test';
+import { createRelease, ensureProduct, loginUser, registerUser } from '../utils/api-client';
+import { E2E, uniqueSuffix } from '../config/test-data';
+
+test.describe('RBAC enforcement', () => {
+});
