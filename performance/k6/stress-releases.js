e6847b8af52ec58eaa74cff305cad3590f60bf3f
Hesara2003 <it23596702@my.sliit.lk>
2026-04-26 11:06:49 +0530

    feat(k6): add ramping-vus scenario definition to stress options

--- performance/k6/stress-releases.js
+++ performance/k6/stress-releases.js
@@ -2,3 +2,12 @@ import http from 'k6/http';
 import { check } from 'k6';
 import { BASE_URL, SHARED_THRESHOLDS, authHeaders } from './common/config.js';
 import { loginAsAdmin } from './common/auth.js';
+
+export const options = {
+  scenarios: {
+    stress_releases: {
+      executor: 'ramping-vus',
+      stages: []
+    }
+  }
+};
