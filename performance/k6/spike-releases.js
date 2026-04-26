f55db7adba39e6d718037d24cf0d63a8d93d716e
Hesara2003 <it23596702@my.sliit.lk>
2026-04-26 11:08:44 +0530

    feat(k6): add spike stages array to options

--- performance/k6/spike-releases.js
+++ performance/k6/spike-releases.js
@@ -2,3 +2,5 @@ import http from 'k6/http';
 import { check } from 'k6';
 import { BASE_URL, SHARED_THRESHOLDS, authHeaders } from './common/config.js';
 import { loginAsAdmin } from './common/auth.js';
+
+export const options = { stages: [] };
