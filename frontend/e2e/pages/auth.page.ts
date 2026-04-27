6daf40d07f3e26f1e2cd6f70e346cb49c1b04825
Shazaanashraff <mohamedshazaan7@gmail.com>
2026-04-28 00:54:53 +0530

    feat(e2e): add form locators to AuthPage

--- frontend/e2e/pages/auth.page.ts
+++ frontend/e2e/pages/auth.page.ts
@@ -2,8 +2,14 @@ import { Page, Locator } from '@playwright/test';
 
 export class AuthPage {
   private page: Page;
+  usernameInput: Locator;
+  passwordInput: Locator;
+  loginButton: Locator;
 
   constructor(page: Page) {
     this.page = page;
+    this.usernameInput = page.locator('input[name="username"]');
+    this.passwordInput = page.locator('input[name="password"]');
+    this.loginButton = page.locator('button[type="submit"]');
   }
 }
